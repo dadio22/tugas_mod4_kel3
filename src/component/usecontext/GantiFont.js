8b0975aad07537a253c0929b24c75dbdfe55e760
yisrel278 <yisrel278@gmail.com>
2021-09-18 19:00:27 +0700

    gantifont

--- src/component/usecontext/GantiFont.js
+++ src/component/usecontext/GantiFont.js
@@ -20,6 +20,7 @@ export default function GantiFont() {
         <Text />
         <button
           className="Button"
+          style={{ margin: 13, fontSize: 20, backgroundColor: "dodgerblue",  color: "white"}}
           onClick={() =>
             setValuefont(
               valuefont === Font.primary ? Font.secondary : Font.primary
@@ -38,7 +39,7 @@ function Text(props) {
   return (
     <p
       className="titleContext"
-      style={{ fontSize: 50, fontFamily: font.value }}
+      style={{ fontSize: 50, fontFamily: font.value, margin: 13 }}
     >
       Halo gais
     </p>
